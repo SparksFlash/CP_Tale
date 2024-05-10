eb7fcc7abca0ab5b34c39e25a2f1124cfae30bdb
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-05-10 21:09:55 +0600

    A

---
+++ is_almost_prime.cpp
@@ -0,0 +1,34 @@
+bool is_prime(int n)
+{
+    int divisors = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            divisors++;
+        }
+    }
+    if (divisors == 2)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool is_almost_prime(int n)
+{
+    for (int i = 0; i <= n; i++)
+    {
+    }
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cin >> num;
+}
